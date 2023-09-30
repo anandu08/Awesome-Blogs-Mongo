1e06304d03a5f35d6797f20d472719a7e4f5110f
Ascend <anandu.ug20@nsut.ac.in>
2023-09-30 17:30:15 +0530

    First

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line
+import './App.css';
+function App() {
+  return (
+  <main>
+
+    <header>
+      <a href="#" className='logo'> Hello</a>
+      <nav>
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+
+
+
+        
+      </nav>
+    </header>
+  </main>
+  );
+}
+
+export default App;
