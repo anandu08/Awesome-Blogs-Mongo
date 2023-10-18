914a31b233c34f2ec0658e2f3abf6d00e785ecd2
Anandu Sateesh <76695947+anandusateeshNSUT@users.noreply.github.com>
2023-10-18 15:37:46 +0530

    Working on some css

--- frontend/src/pages/posts.js
+++ frontend/src/pages/posts.js
@@ -3,26 +3,31 @@ import Post from "./post";
 
 export default function Posts() {
     const [posts, setPosts] = useState([]);
-    useEffect(() => {
 
-        fetch('https://awesome-blogs-server.vercel.app/posts').then(response => {
-            response.json().then(posts => {
-                setPosts(posts);
-            })
-        })
+    const fetchPosts = async () => {
+        try {
+            const response = await fetch('https://awesome-blogs-server.vercel.app/posts');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setPosts(data);
+        } catch (error) {
+            console.error('Error fetching posts:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPosts();
     }, []);
 
-    if (posts.length == 0)
-        return (<> No Posts found, Login to create one!!!</>)
+    if (posts.length === 0) {
+        return <> No Posts found, Login to create one!!!</>;
+    }
+
     return (
         <>
-            {
-                posts.length > 0 && posts.map(post => (<Post {...post} />))
-            }
-
+            {posts.map(post => (<Post key={post._id} {...post} />))}
         </>
-
-
     );
-
 }
