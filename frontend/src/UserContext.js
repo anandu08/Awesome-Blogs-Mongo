d581eb195eaf7cc2f2eaea8370a3c5ca4c600ca6
Anandu Sateesh <76695947+anandusateeshNSUT@users.noreply.github.com>
2023-10-18 17:46:58 +0530

    Update UserContext.js

--- frontend/src/UserContext.js
+++ frontend/src/UserContext.js
@@ -1,13 +1,40 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
-    const [userInfo, setUserInfo] = useState({});
+    const [userInfo, setUserInfo] = useState(() => {
+        const savedUserInfo = JSON.parse(getCookie("userInfo"));
+        return savedUserInfo || {};
+    });
+
+    useEffect(() => {
+        setCookie("userInfo", JSON.stringify(userInfo), 7); // Expires in 7 days
+    }, [userInfo]);
+
+    function setCookie(name, value, days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
+    function getCookie(name) {
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const cookies = decodedCookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i];
+            while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1);
+            }
+            if (cookie.indexOf(name) === 0) {
+                return cookie.substring(name.length, cookie.length);
+            }
+        }
+        return "";
+    }
+
     return (
-
-
         <UserContext.Provider value={{ userInfo, setUserInfo }}>
             {children}
         </UserContext.Provider>
