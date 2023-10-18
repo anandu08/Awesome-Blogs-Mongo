fbdc209780710287b99b4aaa63046842b5a2ce92
Anandu Sateesh <76695947+anandusateeshNSUT@users.noreply.github.com>
2023-10-18 15:39:55 +0530

    Update posts.js

--- frontend/src/pages/posts.js
+++ frontend/src/pages/posts.js
@@ -3,31 +3,26 @@ import Post from "./post";
 
 export default function Posts() {
     const [posts, setPosts] = useState([]);
-
-    const fetchPosts = async () => {
-        try {
-            const response = await fetch('https://awesome-blogs-server.vercel.app/posts');
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            setPosts(data);
-        } catch (error) {
-            console.error('Error fetching posts:', error);
-        }
-    };
-
     useEffect(() => {
-        fetchPosts();
+
+      fetch('https://awesome-blogs-server.vercel.app/posts').then(response => {
+            response.json().then(posts => {
+                setPosts(posts);
+            })
+        })
     }, []);
 
-    if (posts.length === 0) {
-        return <> No Posts found, Login to create one!!!</>;
-    }
-
+    if (posts.length == 0)
+        return (<> No Posts found, Login to create one!!!</>)
     return (
         <>
-            {posts.map(post => (<Post key={post._id} {...post} />))}
+            {
+                posts.length > 0 && posts.map(post => (<Post {...post} />))
+            }
+
         </>
+
+
     );
+
 }
