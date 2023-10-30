904fd6356a8fb41353b35b3f1ea4b46c153a78a1
Ascend <anandu.ug20@nsut.ac.in>
2023-10-30 16:07:24 +0530

    Latest

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,25 +1,21 @@
 import { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../UserContext";
-import { LoadingContext } from "../LoadingContext";
 
 export default function Login() {
-    const { setLoading } = useContext(LoadingContext);
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [redirect, setRedirect] = useState(false);
     const { setUserInfo } = useContext(UserContext)
     async function loginBackend(ev) {
         ev.preventDefault();
-        setLoading(true);
-        const response = await fetch('https://awesome-blogs-server.vercel.app/login', {
+
+        const response = await fetch('http://localhost:4000/login', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
             credentials: 'include',
         })
-
-        setLoading(false)
         if (response.ok) {
             response.json().then(userInfo => {
                 setUserInfo(userInfo);
@@ -40,6 +36,7 @@ export default function Login() {
 
 
         <form action="" className="login" onSubmit={loginBackend}>
+
             <input type="text" placeholder="username"
                 value={username}
                 onChange={event => { setUsername(event.target.value) }} />
@@ -48,5 +45,6 @@ export default function Login() {
                 onChange={event => { setPassword(event.target.value) }} />
             <button>Login</button>
         </form>
+
     );
 }
